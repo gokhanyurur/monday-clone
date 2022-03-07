dbf5655daa1c1ea7481561aad1e3dc92d210a48d
Gökhan Yürür <yururg@gmail.com>
2022-03-07 20:11:27 +0200

    Add linter config (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,45 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+  },
+  rules: {
+    "vue/no-unused-vars": "error",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/multi-word-component-names": 0,
+    "vue/component-name-in-template-casing": [
+      "error",
+      "PascalCase",
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+      },
+    ],
+  },
+  globals: {
+    $nuxt: true,
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    requireConfigFile: false,
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/recommended",
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+};
