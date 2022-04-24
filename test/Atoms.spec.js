09bd34870ec7b15fd00b851a64bbc7d40cc4a721
Gokhan Yurur <yururg@gmail.com>
2022-04-24 16:54:45 +0300

    Unit tests of the header part 1

--- test/Atoms.spec.js
+++ test/Atoms.spec.js
@@ -2,6 +2,9 @@ import { mount } from "@vue/test-utils";
 import Button from "@/components/atoms/Button.vue";
 import Logo from "@/components/atoms/Logo.vue";
 import DropdownItem from "@/components/atoms/DropdownItem.vue";
+import MInput from "@/components/atoms/MInput";
+import Separator from "@/components/atoms/Separator";
+import SquareTag from "@/components/atoms/SquareTag";
 
 describe("Button Atom", () => {
   test("Check props", () => {
@@ -9,11 +12,17 @@ describe("Button Atom", () => {
       propsData: {
         classes: "test__class",
         text: "Button Label",
+        size: "lg",
+        disableAnimation: true,
       },
     });
     expect(wrapper.vm).toBeTruthy();
 
     expect(wrapper.find("span").text()).toBe("Button Label");
+    expect(wrapper.find("span").classes().includes("text-lg")).toBe(true);
+    expect(
+      wrapper.findAll("span").at(1).classes().includes("disable-animation")
+    ).toBe(true);
     expect(wrapper.find("button").classes().includes("test__class")).toBe(true);
   });
 
@@ -49,3 +58,59 @@ describe("Logo Atom", () => {
     expect(wrapper.vm).toBeTruthy();
   });
 });
+
+describe("MInput Atom", () => {
+  test("test props", () => {
+    const wrapper = mount(MInput, {
+      propsData: {
+        placeholder: "Test placeholder",
+        classes: "test-input-class",
+      },
+    });
+    expect(wrapper.vm).toBeTruthy();
+
+    expect(wrapper.find("input").attributes().placeholder).toBe(
+      "Test placeholder"
+    );
+    expect(wrapper.find("input").classes().includes("test-input-class")).toBe(
+      true
+    );
+  });
+});
+
+describe("Separator Atom", () => {
+  test("test props", () => {
+    const wrapper = mount(Separator, {
+      propsData: {
+        text: "Or",
+        classes: "test-separator-wrapper",
+      },
+    });
+    expect(wrapper.vm).toBeTruthy();
+
+    expect(
+      wrapper.find("div").classes().includes("test-separator-wrapper")
+    ).toBe(true);
+    expect(wrapper.findAll("span").at(0).attributes().class).toBe(
+      "separator__line"
+    );
+    expect(wrapper.findAll("span").at(1).text()).toBe("Or");
+    expect(wrapper.findAll("span").at(2).attributes().class).toBe(
+      "separator__line"
+    );
+  });
+});
+
+// describe("Square Tag Atom", () => {
+//   test("test props", () => {
+//     const wrapper = mount(SquareTag, {
+//       propsData: {
+//         title: "Test tag",
+//         imgSrc: "dev.png",
+//         color: "#ffffff",
+//       },
+//     });
+//     expect(wrapper.vm).toBeTruthy();
+//     console.log(wrapper.html());
+//   });
+// });
